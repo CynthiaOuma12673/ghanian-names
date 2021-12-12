84b6565bd057d7a8fe014eaf6723979fae423c1a
CynthiaOuma12673 <cynthia.ouma@student.moringaschool.com>
2021-12-12 19:05:39 +0300

    it will change the proof layout

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,6 @@
-<select name="day" id="dayOfMonth" class="form-control" type="number"></select>
+var femaleNames = ["Akosua", "Adwoa", "Abenna", "Akua", "Yaa", "Afua", "Ama"];
+var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+var dayOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var dayValue = [d,DD,MM,YY,CC,yr]
+
+
