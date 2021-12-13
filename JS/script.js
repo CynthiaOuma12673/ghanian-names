50427c1531947aadee43716d9029c387b4d0c207
CynthiaOuma12673 <cynthia.ouma@student.moringaschool.com>
2021-12-13 23:01:55 +0300

    last commit

--- JS/script.js
+++ JS/script.js
@@ -4,3 +4,29 @@ var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 var dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 var dayValue, d, DD, MM, YY, CC, yr;
 
+var form = document.getElementById('forms');
+form.addEventListener('submit', (event)=> {
+    event.preventDefault();
+    var gndr = document.querySelector('input[name="gndr"]:checked')
+    var birthDate = document.getElementById('date').value;
+
+    birthDate = new Date (birthDate);
+
+    var dayOfWeek = birthDate.getDay();
+
+    var akanName;
+    femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+    maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+
+    if (gndr==='male') {
+        akanName = maleNames[dayOfWeek];
+
+    }
+    else {
+        akanName = femaleNames[dayOfWeek]
+    }
+
+    document.getElementById("results").innerHTML = '<h3>You got it</h3><p>Your Akan Name is </p> <h4>${akanName}</h4>'
+    document.getElementById("forms").reset();
+
+})
